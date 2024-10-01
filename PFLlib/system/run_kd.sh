93ed4b68e3f9ea84052ff0636ac91bb2b92adbc6
phamdinhdat-ai <datolg123@gmail.com>
2024-10-01 15:56:08 +0700

    restore fedkd

--- PFLlib/system/run_kd.sh
+++ PFLlib/system/run_kd.sh
@@ -13,14 +13,14 @@
 #SBATCH --mail-user=20010736@st.phenikaa-uni.edu.vn
 
 # test on HAR dataset
-python main.py -data HAR -m harcnn -algo FedKD -gr 2000 -did 0 -nc 30 -lbs 64 -nb 6 -jr 0.4 -ls 5 -ld True  -dev cuda:0
+python main.py -data HAR -m harcnn -algo FedKD -gr 2000 -did 0 -nc 30 -lbs 64 -nb 6 -jr 0.4 -ls 5 -ld True  -dev cuda
 
 
-python main.py -data HAR -m transformer -algo FedKD -gr 2000 -did 0 -nc 30 -lbs 64 -nb 6  -jr 0.4 -ls 5 -ld True -dev cuda:0
+python main.py -data HAR -m transformer -algo FedKD -gr 2000 -did 0 -nc 30 -lbs 64 -nb 6  -jr 0.4 -ls 5 -ld True -dev cuda
 
 
 # test on SLEEP dataset
-python main.py -data SLEEP -m harcnn -algo FedKD -gr 2000 -did 0 -nc 15 -lbs 64 -nb 12 -jr 0.4 -ls 5 -ld True -dev cuda:0
+python main.py -data SLEEP -m harcnn -algo FedKD -gr 2000 -did 0 -nc 15 -lbs 64 -nb 12 -jr 0.4 -ls 5 -ld True -dev cuda
 
 
-python main.py -data SLEEP -m transformer -algo FedKD -gr 2000 -did 0 -nc 15 -lbs 64 -nb 12  -jr 0.4 -ls 5 -ld True -dev cuda:0
+python main.py -data SLEEP -m transformer -algo FedKD -gr 2000 -did 0 -nc 15 -lbs 64 -nb 12  -jr 0.4 -ls 5 -ld True -dev cuda
